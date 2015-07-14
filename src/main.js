0d073474c14c67d834f00a771b8f81dd53c97989
Michael Atkinson <michael@philomathery.com>
2015-07-14 09:05:52 -0500

    more numbers go up

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
+var state = {};
+
 function row() {
-  var r = document.getElementById('rowed');
-  r.textContent = parseInt(r.textContent, 10) + 1;
+  state.accSeaworth++;
+  document.getElementById('distance').textContent = ++state.distance;
+}
+
+function seaworth() {
+  if (state.accSeaworth >= state.nextSeaworth) {
+    state.accSeaworth = 0;
+    // FIXME extract constants to configuration
+    // TODO extract pricing function
+    state.nextSeaworth = 2 + Math.floor(Math.pow(1.05, ++state.seaworth));
+    document.getElementById('seaworth').textContent = state.seaworth;
+  }
 }
 
 document.getElementById('row').addEventListener('click', function () {
   row();
+  seaworth();
 });
+
+(function () {
+  state.distance = 0;
+  state.seaworth = 0;
+  state.accSeaworth = 0;
+  state.nextSeaworth = 3;
+}());
