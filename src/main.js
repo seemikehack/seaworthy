7d63b32768627adab521937edc87f1226f21ccad
Michael Atkinson <michael@philomathery.com>
2015-07-15 01:17:28 -0500

    wrapping main js in a module

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
-var state = {};
+var seaworthy = (function () {
+  var state = {};
 
-function row() {
-  state.accSeaworth++;
-  $('#distance').text(++state.distance);
-}
-
-function seaworth() {
-  if (state.accSeaworth >= state.nextSeaworth) {
-    state.accSeaworth = 0;
-    // FIXME extract constants to configuration
-    // TODO extract pricing function
-    state.nextSeaworth = 2 + Math.floor(Math.pow(1.05, ++state.seaworth));
-    $('#seaworth').text(state.seaworth);
+  function row() {
+    state.accSeaworth++;
+    $('#distance').text(++state.distance);
   }
-}
 
-$('#row').click(function () {
-  row();
-  seaworth();
-});
+  function seaworth() {
+    if (state.accSeaworth >= state.nextSeaworth) {
+      state.accSeaworth = 0;
+      // FIXME extract constants to configuration
+      // TODO extract pricing function
+      state.nextSeaworth = 2 + Math.floor(Math.pow(1.05, ++state.seaworth));
+      $('#seaworth').text(state.seaworth);
+    }
+  }
 
-(function () {
-  state.distance = 0;
-  state.seaworth = 0;
-  state.accSeaworth = 0;
-  state.nextSeaworth = 3;
+  $('#row').click(function () {
+    row();
+    seaworth();
+  });
+
+  (function () {
+    state.distance = 0;
+    state.seaworth = 0;
+    state.accSeaworth = 0;
+    state.nextSeaworth = 3;
+  }());
 }());
